aa744dba758fba817c93a0569b9b3a3470857847
Harika Rachamalla <92622089+harika0293@users.noreply.github.com>
2022-02-18 17:20:15 +0530

    Add files via upload

---
+++ New folder/src/MyComponents/About.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <>
+        <div className="about-container">
+            
+            <p>To Do is a task management app to help you stay organized and manage your day-to-day. You can use Microsoft To Do to make shopping lists or task lists, take notes, record collections, plan an event, or set reminders to increase your productivity and focus on what matters to you. Microsoft To Do makes it easy to stay organized and manage your life.
+
+Accomplish what's meaningful and important to you each day with My Day and Suggestions, your personalized daily planner tools. Intelligent suggestions recommend tasks from across your lists that may be relevant for the day. From grocery lists to housecleaning routines, daily tasks are simple with To Do. When you're quickly switching between contexts and tasks, it's important to have tools that help you achieve what matters. To Do helps you quickly capture and retrieve your lists and tasks across devices and between multiple accounts.
+
+Capture tasks from different apps and services and sync them with To Do. Flag emails as tasks in Outlook or any email account hosted by Microsoft, add to lists with Cortana, and see tasks assigned to you from Planner. Get peace of mind knowing that your tasks and lists are hosted on the Microsoft 365 service – one of the most reliable services with industry-leading security offerings.
+
+This My-To Do’s modern, easy-to-use experience makes your lists unique, with customizable features like emoji in lists, colorful themes, dark mode, and more. Plus, shared lists help you stay connected and collaborate with family, friends, and colleagues. <br />
+
+<h4>Daily planner</h4><br />
+
+• A daily planner personalized with suggested tasks: My Day<br />
+• To do lists are accessible anywhere, on any device<br />
+• Share lists and assign tasks with your friends, family, colleagues, and classmates<br />
+• Task management features break your tasks into manageable steps<br />
+• Take notes to add to any task<br />
+• Group lists together by topic or project<br />
+
+<h4>Task manager</h4><br />
+
+• Add reminders, tasks and lists with the To Do widget<br />
+• Daily organizer personalized with bold and colorful backgrounds<br />
+• Reminders with one-time or recurring due dates<br />
+• Make task lists and switch between school, work, and personal lists<br />
+• Attach up to 25 MB of files to any task<br />
+
+<h4>To Do lists for any purpose</h4><br />
+
+• Bill planner<br />
+• Shopping list<br />
+• Reminders<br />
+• Task management<br />
+• Take notes<br />
+• And more</p>
+        </div>
+        </>
+    )
+}
